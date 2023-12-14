3d6eb9fd26aeaeb0f0a79168bc42f1e49563637d
tasnime <tasnim.fr2008@gmail.com>
2023-12-14 07:30:18 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fonction
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
