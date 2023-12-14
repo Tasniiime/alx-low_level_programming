fab21433bab2b8e890919d3ace7912f366eac58d
tasnime <tasnim.fr2008@gmail.com>
2023-12-14 09:22:22 -0800

    task 18

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main
+ *
+ *return: 1
+ */
+
+int main (void)
+{
+	write(2, "and that piece of art in useful\ -Dora korpar, 2015-10-19", 59)
+	retun (1)
+}	
