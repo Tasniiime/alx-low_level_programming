5a55cf6ab1337432ff335d8370cd4cb916bc30fa
tasnime <tasnim.fr2008@gmail.com>
2023-12-14 09:52:59 -0800

    task 18

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main
+ * main - Entry point
  *
- *return: 1
+ * Return: Always 1 (Error)
  */
-
-int main (void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	retun (1);
-}	
+	return (1);
+}
