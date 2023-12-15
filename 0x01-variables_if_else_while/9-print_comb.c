4ba3155275091880679af926e2dd1dd6b9c50c70
tasnime <tasnim.fr2008@gmail.com>
2023-12-15 13:59:19 -0800

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
@@ -11,9 +12,14 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	return (0);
 }
