bcfeeba176e4e01f921e2892136fd1128a431dfd
tasnime <tasnim.fr2008@gmail.com>
2023-12-21 09:37:17 -0800

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,15 +1,27 @@
-#include "../main.h"
+#include "main.h"
 
 /**
- * main - check the code for ALX School students
+ * print_square - prints a square of size size
+ * @size: size of the square
  *
- * Return: Always 0 (Success)
-*/
+ * Return: void
+ */
 
-int main(void)
+
+void print_square(int size)
 {
-	print_square(2);
-	print_square(10);
-	print_square(0);
-	return (0);
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
 }
