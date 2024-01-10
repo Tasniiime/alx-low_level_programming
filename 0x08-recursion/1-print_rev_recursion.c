983cc8b9b6bed3bbbb78e28c515b81d8418e7dc1
tasnime <tasnim.fr2008@gmail.com>
2024-01-10 03:41:44 -0800

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _print_rev_recursion - reverse
- * @s: pointer to string params
- *
+ *_print_rev_recursion - prints a string in reverse
+ *@s: pointer block of memory to fill
+ *Return: void
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	
+if (*s) /*If statement*/
+{
+_print_rev_recursion(s + 1); /*add s*/
+_putchar(*s); /*print s*/
+}
+}
